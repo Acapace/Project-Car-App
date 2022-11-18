218abc2d4939a6eca60898282e20cb1fff3ed7cc
Ashley Capace <acapace@yahoo.com>
2022-11-18 12:35:19 -0500

    created folders/files

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ const express = require ('express');
 const mongoose = require('mongoose');
 const app = express();
 
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+app.get('/', (req, res) =>{
+    res.send('hi');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
 let PORT = 3000
 if(process.env.PORT){
     PORT = process.env.PORT
@@ -12,6 +35,10 @@ app.listen(PORT, () => {
     console.log('listening....');
 });
 
+
 mongoose.connect('mongodb+srv://Acapace:!Acura123456@cluster0.cldlibi.mongodb.net/?retryWrites=true&w=majority', () =>{
     console.log('connected to mongo');
 });
+
+
+/////////////////////////////////////////////////////////////////////////////////////
