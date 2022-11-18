88ae7bd76b0380093c792a28f2c769fb944c6a94
Ashley Capace <acapace@yahoo.com>
2022-11-17 19:03:52 -0500

    connecting to mongo

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const mongoose = require('mongoose');
 const app = express();
 
 let PORT = 3000
@@ -14,3 +15,7 @@ app.get('/', (req, res) =>{
 app.listen(PORT, () => {
     console.log('listening....');
 });
+
+mongoose.connect('mongodb+srv://Acapace:!Acuraitr62290@cluster0.cldlibi.mongodb.net/?retryWrites=true&w=majority', () =>{
+    console.log('connected to mongo');
+})
